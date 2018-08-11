9cd3b8e2ce923882fb8699d69a2101e2426a20d9
patrickatcw <pj8392@yahoo.com>
2018-08-11 12:12:40 -0400

    set up initial structure for example to do, update gitignore file

---
+++ src/com/me/Main.java
@@ -0,0 +1,12 @@
+package com.me;
+
+//http://www.java2s.com/Tutorials/Java/Java_io/0600__Java_io_Random_Access_Files.htm
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+}
